5b0570b9c4f192908257e8d130343f651bffa5c3
kikimetal <reezafratzitz2@gmail.com>
2017-12-24 12:39:38 +0900

    install redux-thunk

--- src/js/AppContainer.jsx
+++ src/js/AppContainer.jsx
@@ -9,18 +9,27 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { ConnectedRouter, routerMiddleware, routerReducer, push } from 'react-router-redux'
 import { createBrowserHistory } from "history"
+
+import thunk from "redux-thunk"
+
 const history = createBrowserHistory()
-const middleware = routerMiddleware(history)
+const middlewares = [
+  routerMiddleware(history),
+  thunk,
+]
 
 // modules
 import reducers from "./modules/reducers"
+
+const initialState = undefined
 
 const store = createStore(
   combineReducers({
     ...reducers,
     router: routerReducer,
   }),
-  applyMiddleware(middleware)
+  initialState,
+  applyMiddleware(...middlewares)
 )
 
 export default class AppContainer extends React.Component{
