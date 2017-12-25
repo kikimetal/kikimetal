af9b1627bbcb126afcf50fe30f159716ed975c43
kikimetal <reezafratzitz2@gmail.com>
2017-12-25 22:27:58 +0900

    汎用的なfetchアクションを追加したい(途中)

--- src/js/modules/action.js
+++ src/js/modules/action.js
@@ -16,3 +16,22 @@ export const setWebsitesData = data => ({
   type: "SET_DATA",
   data,
 })
+
+// fetch
+// TODO
+// export const fetchJSON = (url, ...actions) => {
+//   return (dispatch) => {
+//     fetch(url)
+//       .then((response) => {
+//         if(!response.ok) {
+//           throw Error(response.statusText);
+//         }
+//         dispatch(loadComments(false));
+//
+//         return response;
+//       })
+//       .then((response) => response.json())
+//       .then((comments) => dispatch(fetchCommentsSuccess(comments)))
+//       .catch(() => dispatch(getCommentsError(true)));
+//   }
+// }
