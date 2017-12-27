16b091eac6cf1c680a13e707f9231199b47b32bf
kikimetal <reezafratzitz2@gmail.com>
2017-12-27 18:38:48 +0900

    微修正

--- src/js/components/LazyLoadImg.jsx
+++ src/js/components/LazyLoadImg.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 
 export default class LazyLoadImg extends React.Component{
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state = {
       isShowPlaceholder: true,
     }
