29bcee920359e3f2eb6e6560c9ec581a38d0b2c6
kikimetal <reezafratzitz2@gmail.com>
2018-01-04 01:52:55 +0900

    defaultProps整理

--- src/js/components/Bg.jsx
+++ src/js/components/Bg.jsx
@@ -1,16 +1,30 @@
 import React from "react"
+import getRandomInt from "../functions/getRandomInt"
 
-const Bg = ({ imgsrc, size, position, scale, className }) => {
+const Bg = ({ color, imgsrc, size, position, scale, className }) => {
+  // const arr = new Array(11).fill(null)
+  // const dStyle = document.documentElement.style
+  // arr.forEach((value, i) => {
+  //   dStyle.setProperty(`--bg-w${i + 1}`, `${(i + 1) * (13 - i / 2)}%`)
+  // })
   return (
     <div
-      className={`Bg ${className ? className : ""}`}
+      className={`Bg ${className}`}
       style={{
-        backgroundImage: `url("${imgsrc}")`,
-        backgroundSize: size || "contain",
-        backgroundPosition: position || "center",
-        transform: `scale(${scale || 1.2})`,
+        backgroundColor: color,
+        // backgroundImage: `url("${imgsrc}")`,
+        backgroundSize: size,
+        backgroundPosition: position,
+        transform: `scale(${scale})`,
       }}></div>
   )
+}
+Bg.defaultProps = {
+  className: "",
+  color: "transparent",
+  size: "cover",
+  position: "center",
+  scale: 1,
 }
 
 export default Bg
